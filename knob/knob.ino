18760da9c5f5e037c306d2c4f3128162e1103808
Jonathan <jonathanxu25@gmail.com>
2018-12-29 22:54:34 -0500

    added sensor knob navigation and changed label designs

--- knob/knob.ino
+++ knob/knob.ino
@@ -1,12 +1,24 @@
+// Include libraries
+//#include <SocketIoClient.h>
 #include <SPI.h>
 #include <Tlv493d.h>
-
 #include <Wire.h>
 
+//Pin declarations
 int output1 = D5;
 int output2 = D6;
-int source = D7;
-int charger = D8;
+int sourceRelay = D7; //High for outlet, low for battery
+int chargerRelay = D8; //High for charge, low for disconnect charger
+int outletSensor = D3;
+
+// Switch statuses
+boolean s1 = true;
+boolean s2 = true;
+int powerMode = 1; //1 for auto, 2 for battery, 3 for outlet
+
+// Network login
+const char* ssid = "newhomoe";
+const char* password = "maolan123";
 
 // Tlv493d Opject
 Tlv493d Tlv493dMagnetic3DSensor = Tlv493d();
@@ -24,30 +36,57 @@ void setup() {
 
   pinMode(output1, OUTPUT);
   pinMode(output2, OUTPUT);
-  pinMode(source, OUTPUT);
-  pinMode(charger, OUTPUT);
+  pinMode(sourceRelay, OUTPUT);
+  pinMode(chargerRelay, OUTPUT);
+  pinMode(outletSensor, INPUT);
   
   digitalWrite(output1, HIGH);
   digitalWrite(output2, HIGH);
-  digitalWrite(source, HIGH);
-  digitalWrite(charger, HIGH);
+  digitalWrite(sourceRelay, HIGH); 
+  digitalWrite(chargerRelay, LOW);
 }
-
 
 void loop() {
   delay(Tlv493dMagnetic3DSensor.getMeasurementDelay());
   Tlv493dMagnetic3DSensor.updateData();
 
   double mag = Tlv493dMagnetic3DSensor.getAmount();
-  double azimuth = Tlv493dMagnetic3DSensor.getAzimuth();
+  double azimuth = (Tlv493dMagnetic3DSensor.getAzimuth() + 3.14)*15.9;
   double polar = Tlv493dMagnetic3DSensor.getPolar();
 
-  if(mag > 55){
-    Serial.print("Pressed ");
-  } else {
-    Serial.print("Not pressed (");
+  if(mag > 45){ //Pressed
+    Serial.print("Pressed: ");
+     if(azimuth < 20){ //Socket2
+      Serial.println(" socket2");
+      s2 = !s2;
+      digitalWrite(output2, s2);
+    } else if(azimuth <40){ //Socket1
+      Serial.println(" socket1");
+      s1 = !s1;
+      digitalWrite(output1, s1);
+    } else if(azimuth < 60){ //battery
+      Serial.println(" battery");
+      powerMode = 2;
+    } else if(azimuth < 80){ //Auto
+      Serial.println(" auto");
+      powerMode = 1;
+    } else { //Outlet
+      Serial.println(" outlet");
+      powerMode = 3;
+    }
+    delay(500);
+  } else { //Not pressed
+    Serial.println("Not pressed");
   }
-  Serial.print(mag);
-  Serial.print(") Angle: ");
-  Serial.println(azimuth);
+
+  if (powerMode == 1){
+    digitalWrite(sourceRelay, digitalRead(outletSensor));
+  } else if (powerMode == 2){
+    digitalWrite(sourceRelay, LOW);
+  } else if (powerMode == 3){
+    digitalWrite(sourceRelay, HIGH);
+  }
+
+  Serial.println(digitalRead(outletSensor));
+  
 }
